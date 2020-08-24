6a1821d90e2242bc23f9f43161dd73308d65cf55
ulises jimenez <jiaupjp@gmail.com>
2020-08-24 14:07:03 -0500

    Se crea un archivo de prueba

---
+++ SnifferApp/SnifferApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnifferApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
